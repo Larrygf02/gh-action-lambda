902396b0710f920ec058ee28f1d71e840e213d19
Larrygf02 <raulgf_02@hotmail.com>
2023-05-07 00:35:40 -0500

    initial commit

---
+++ index.py
@@ -0,0 +1,8 @@
+
+def lambda_handler(event, context):
+    print("start function")
+    print("end function")
+    return {
+        'statusCode': 200,
+        'body': 'Github action lambda'
+    }
